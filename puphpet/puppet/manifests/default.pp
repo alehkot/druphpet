d47f5ad5ed2bb8e5ee50ebeb2f09e9927fd248ca
Aleh Kot <kot.oleg@gmail.com>
2014-05-29 08:27:58 +0300

    Add RabbitMQ.

--- puphpet/puppet/manifests/default.pp
+++ puphpet/puppet/manifests/default.pp
@@ -692,3 +692,35 @@ if has_key($mailcatcher_values, 'install') and $mailcatcher_values['install'] ==
     command => "/usr/local/bin/mailcatcher --http-ip=${mailcatcher_host_ip} --http-port=${mailcatcher_host_port}",    
   }    
 }
+
+# Begin rabbitmq
+
+if $rabbitmq_values == undef {
+  $rabbitmq_values = hiera('rabbitmq', false)
+} if $php_values == undef {
+  $php_values = hiera('php', false)
+} if $apache_values == undef {
+  $apache_values = hiera('apache', false)
+} if $nginx_values == undef {
+  $nginx_values = hiera('nginx', false)
+}
+
+if hash_key_equals($apache_values, 'install', 1)
+  or hash_key_equals($nginx_values, 'install', 1)
+{
+  $rabbitmq_webserver_restart = true
+} else {
+  $rabbitmq_webserver_restart = false
+}
+
+if hash_key_equals($rabbitmq_values, 'install', 1) {
+  create_resources('class', { 'rabbitmq' => $rabbitmq_values['settings'] })
+
+  if hash_key_equals($php_values, 'install', 1) and ! defined(Php::Pecl::Module['amqp']) {
+    php::pecl::module { 'amqp':
+      use_package         => false,
+      service_autorestart => $rabbitmq_webserver_restart,
+      require             => Class['rabbitmq']
+    }
+  }
+}  
